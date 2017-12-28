a180806d322e00cc4938fe5045223c25ba724da3
Chris Huatulco <chrishuatulco@gmail.com>
2017-12-28 13:16:17 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log(err);
+    };
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'My Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome to my home page now'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page!!');
+});
+
+app.get('/help', (req, res) => {
+  res.render('help.hbs', {
+    pageTitle: 'My Help Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
